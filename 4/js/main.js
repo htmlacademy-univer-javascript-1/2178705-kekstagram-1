3dc86e2516388f648471edca793d8fe1fa9a0ee8
Keks <keks@htmlacademy.ru>
2022-10-10 08:51:33 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,7 +1,7 @@
 ///Печать массива в консоли
 // eslint-disable-next-line no-console
-import {objects} from './data.js';
+import {getObjects} from './data.js';
 import './util.js';
 
 // eslint-disable-next-line no-console
-console.log(objects());
+console.log(getObjects());
