c38d77f4962eec4d0e854b2de266ef3f496bcd74
SHWEDUS <krylov1243@gmail.com>
2022-10-24 11:03:25 +0500

    fix render photo

--- js/render.js
+++ js/render.js
@@ -3,18 +3,20 @@ import {resultArray} from './data.js';
 const pictureTemplate = document.querySelector('#picture').content;
 const newPictureTemplate = pictureTemplate.querySelector('.picture');
 const photosContainer = document.querySelector('.pictures');
-const fragment = document.createDocumentFragment();
 
-const renderPhoto = function ({url, likes, comments}){
+const getPhotoElement = function ({url, likes, comments}){
   const clonedPhoto = newPictureTemplate.cloneNode(true);
   clonedPhoto.querySelector('img').src = url;
   clonedPhoto.querySelector('.picture__likes').textContent = likes;
-  clonedPhoto.querySelector('.picture__comments').textContent = comments;
+  clonedPhoto.querySelector('.picture__comments').textContent = comments.length;
+
+  return clonedPhoto;
 };
 
 const renderPhotos = function (photos){
+  const fragment = document.createDocumentFragment();
   for (let i = 0; i<photos.length; i++){
-    renderPhoto(photos[i]);
+    fragment.appendChild(getPhotoElement(photos[i]));
   }
   photosContainer.appendChild(fragment);
 };
