069b156aaa27e831f72fdc9cffcb6c2d89e612ea
Keks <keks@htmlacademy.ru>
2022-11-10 15:13:05 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,11 @@
+///Печать массива в консоли
+// eslint-disable-next-line no-console
+/* eslint-disable no-unused-vars */
+import {resultArray} from './data.js';
+import './util.js';
+import {photosContainer} from './render.js';
+import {showBigPicture} from './render-big-pictures.js';
+
+
+// eslint-disable-next-line no-console
+
